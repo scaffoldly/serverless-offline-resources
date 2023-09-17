bbcf60a4de73493437bbbf5e817925e1bb6ff87b
Christian Nuss <christian.nuss@gmail.com>
2023-09-17 14:56:26 -0700

    special handling for localstack

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const _ = require("lodash");
 const BbPromise = require("bluebird");
 const AWS = require("aws-sdk");
 
+const LOCALSTACK_ENDPOINT = "http://localhost.localstack.cloud:4566";
+
 class ServerlessOfflineResources {
   constructor(serverless, options) {
     this.serverless = serverless;
@@ -24,29 +26,37 @@ class ServerlessOfflineResources {
   get endpoint() {
     const config =
       (this.service.custom && this.service.custom["offline-resources"]) || {};
-    const port = _.get(config, "endpoint", "http://localhost:4566");
-    return port;
+    return _.get(config, "endpoint", LOCALSTACK_ENDPOINT);
   }
 
   get region() {
     const config =
       (this.service.custom && this.service.custom["offline-resources"]) || {};
-    const port = _.get(config, "region", "us-east-1");
-    return port;
+    return _.get(config, "region", "us-east-1");
   }
 
   get accessKeyId() {
     const config =
       (this.service.custom && this.service.custom["offline-resources"]) || {};
-    const port = _.get(config, "accessKeyId", "test");
-    return port;
+    const val = _.get(config, "accessKeyId", undefined);
+
+    if (!val && this.endpoint === LOCALSTACK_ENDPOINT) {
+      return "test";
+    }
+
+    return val;
   }
 
   get secretAccessKey() {
     const config =
       (this.service.custom && this.service.custom["offline-resources"]) || {};
-    const port = _.get(config, "secretAccessKey", "test");
-    return port;
+    const val = _.get(config, "secretAccessKey", undefined);
+
+    if (!val && this.endpoint === LOCALSTACK_ENDPOINT) {
+      return "test";
+    }
+
+    return val;
   }
 
   get stage() {
